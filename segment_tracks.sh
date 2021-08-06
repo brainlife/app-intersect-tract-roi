c2cfdfd70ee598acd8150943667fd9f3e71e1fc9
Brad Caron <bacaron@iu.edu>
2021-08-05 23:30:31 -0400

    reason: initial commit

--- segment_tracks.sh
+++ segment_tracks.sh
@@ -12,12 +12,10 @@ intersect_options=`jq -r '.intersect_type' config.json`
 inverse=`jq -r '.inverse' config.json`
 endpoints_only=`jq -r '.endpoints_only' config.json`
 rois=`jq -r '.rois' config.json`
+track=`jq -r '.track' config.json`
 
 # need to set this as a multiline array
 intersect_options=($intersect_options)
-
-# identify the parent tracks
-tracks=(`ls *track*.tck`)
 
 # this makes for loops with multi lines possible and much easier
 roiArray=()
@@ -46,61 +44,56 @@ else
 	endpoint_string="traverse"
 fi
 
-# loop through tracks
-for (( it=0; it<${#tracks[@]}; ++it ));
+# loop through roi lines
+for (( irn=0; irn<${#roiArray[@]}; ++irn ));
 do
-	echo "track ${it}"
-	# loop through roi lines
-	for (( irn=0; irn<${#roiArray[@]}; ++irn ));
-	do
-		# set rois as tmparray to easier parse. also set intersect option for this roi
-		tmparray=(${roiArray[$irn]})
-		intersect=${intersect_options[$irn]}
+	# set rois as tmparray to easier parse. also set intersect option for this roi
+	tmparray=(${roiArray[$irn]})
+	intersect=${intersect_options[$irn]}
 
-		# capture what the intersect option should be for the 
-		if [[  ${intersect} == 'exclude' ]]; then
-			reg_line='-exclude'
-			reg_string="exclude"
-		else
-			reg_line='-include'
-			reg_string="include"
-		fi
+	# capture what the intersect option should be for the 
+	if [[  ${intersect} == 'exclude' ]]; then
+		reg_line='-exclude'
+		reg_string="exclude"
+	else
+		reg_line='-include'
+		reg_string="include"
+	fi
 
-		# need to identify when multiple rois are desired. if so, will iteratively go through each roi and extract streamlines. if just a single
-		# roi, will just do the simple extraction once
-		if [[ ${#tmparray[@]} -gt 1 ]]; then
-			filelist=()
-			for (( j=0; j<${#tmparray[@]}; ++j ));
-			do
-				if [[ ${j} -eq 0 ]]; then
-					tmptrack=${tracks[$it]%%.tck}
-					out_name="./tmp/${tracks[$it]%%.tck}_roioptions_${reg_string}_inverseoptions_${inv_string}_rois_${tmparray[$j]}"
-				else
-					tmptrack=${filelist[$j-1]}
-					out_name="${filelist[$j-1]}_${tmparray[$j]}"
-				fi
+	# need to identify when multiple rois are desired. if so, will iteratively go through each roi and extract streamlines. if just a single
+	# roi, will just do the simple extraction once
+	if [[ ${#tmparray[@]} -gt 1 ]]; then
+		filelist=()
+		for (( j=0; j<${#tmparray[@]}; ++j ));
+		do
+			if [[ ${j} -eq 0 ]]; then
+				tmptrack=${track}
+				out_name="./tmp/wholebrain_roioptions_${reg_string}_inverseoptions_${inv_string}_rois_${tmparray[$j]}"
+			else
+				tmptrack=${filelist[$j-1]}
+				out_name="${filelist[$j-1]}_${tmparray[$j]}"
+			fi
 
-				roi_path="${rois}/ROI${tmparray[$j]}.nii.gz"
+			roi_path="${rois}/ROI${tmparray[$j]}.nii.gz"
 
-				tckedit ${tmptrack}.tck ${reg_line} ${roi_path} ${inv_line} ${endpoint_line} ${out_name}.tck
+			tckedit ${tmptrack%%.tck}.tck ${reg_line} ${roi_path} ${inv_line} ${endpoint_line} ${out_name}.tck
 
-				if [[ ${j} -gt 0 ]]; then
-					rm -rf ${tmptrack}.tck
-				fi
+			if [[ ${j} -gt 0 ]]; then
+				rm -rf ${tmptrack}.tck
+			fi
 
-				filelist+=("${out_name}")
-			done
-		else
-			roi_path="${rois}/ROI${tmparray}.nii.gz"
-			out_name="./tmp/${tracks[$it]%%.tck}_roioptions_${reg_string}_inverseoptions_${inv_string}_rois_${tmparray}.tck"
+			filelist+=("${out_name}")
+		done
+	else
+		roi_path="${rois}/ROI${tmparray}.nii.gz"
+		out_name="./tmp/wholebrain_roioptions_${reg_string}_inverseoptions_${inv_string}_rois_${tmparray}.tck"
 
-			tckedit ${tracks[$it]} ${reg_line} ${roi_path} ${inv_line} ${endpoint_line} ${out_name}
-		fi
-	done
+		tckedit ${track} ${reg_line} ${roi_path} ${inv_line} ${endpoint_line} ${out_name}
+	fi
 done
 
 # remove parent tracks, move segmented tracks to pwd and remove tmp dir
-rm -rf `echo ${tracks[*]}`
+# rm -rf `echo ${tracks[*]}`
 mv tmp/* ./
 rm -rf tmp
 
@@ -116,15 +109,15 @@ do
 done
 
 # merge final tcks into single tck
-tcks=`ls *track*.tck`
+tcks=`ls *wholebrain*.tck`
 tckedit ${tcks} ./track/track.tck
 echo ${tcks} >> track_names.txt
 
 # final check to make sure the final tck was made. if not will exit here
 if [ ! -f ./track/track.tck ]; then
 	echo "something went wrong. check derivatives and logs"
-	exit 1
+	# exit 1
 else
 	echo "segmentation complete. creating wmc structure"
-	exit 0
+	# exit 0
 fi
