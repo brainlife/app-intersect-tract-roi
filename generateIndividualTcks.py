20d5a62c77f4b6230c061576ef6d43c2bb537d73
Brad Caron <bacaron@iu.edu>
2021-08-26 11:38:01 -0400

    reason: fix issues

--- generateIndividualTcks.py
+++ generateIndividualTcks.py
@@ -16,16 +16,18 @@ def generateIndividualTcks():
    print("loading data")
    ref = nib.load(config['reference_anatomy'])
    wholebrain = load_tractogram(config['track_path'],ref) 
-   classification = sio.loadmat(config['classification_path'])
+   classification = sio.loadmat(config['classification_path'],squeeze_me=True)
    print("loading data complete")
 
    # obtain tract index
    print("extracting bundles of interest")   
-   class_indices = list(classification['classification'][0]['index'][0])
-   class_names = classification['classification'][0]['names'][0][0]
+   # class_indices = list(classification['classification'][0]['index'][0])
+   class_indices = list(classification['classification'].item()[1])
+   # class_names = classification['classification'][0]['names'][0][0]
+   class_names = classification['classification'].item()[0]
 
    for i in np.unique(class_indices):
-      tract_name = class_names[i-1].tolist()[0]
+      tract_name = class_names[i-1]
       tract_indices = [ f for f in range(len(class_indices)) if class_indices[f] == i ]
       print(tract_name)
       # select tract and save tck
